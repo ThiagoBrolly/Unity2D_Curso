5e1c611b1461813d189a0a589331d4ad18041166
unknown <thiagoex377@gmail.com>
2020-03-02 09:54:42 -0300

    Primeiro commit

---
+++ Chasing.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chasing : MonoBehaviour {
+
+	Animator anim;
+	public bool isWalking;
+	public Transform pointWalkRight;
+	public Transform pointWalkLeft;
+	public Transform pointAttack;
+
+	public bool isWalkingToTheRight;
+	public bool isWalkingToTheLeft;
+	public bool readyToAttack;
+
+	public float AttackRadius;
+	public float WalkingRadius;
+	public float enemySpeed;
+
+	public LayerMask layerPlayer;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponent<Animator>();
+		
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+
+		anim.SetBool("Chasing", isWalking);
+
+		isWalkingToTheRight = Physics2D.OverlapCircle(pointWalkRight.position, WalkingRadius, layerPlayer);
+		isWalkingToTheLeft = Physics2D.OverlapCircle(pointWalkLeft.position, WalkingRadius, layerPlayer);
+		readyToAttack = Physics2D.OverlapCircle(pointAttack.position, AttackRadius, layerPlayer);
+
+		if(isWalkingToTheLeft || isWalkingToTheRight){
+			isWalking = true;
+		} else{
+			isWalking = false;
+		}
+
+		if(isWalkingToTheRight){
+			gameObject.GetComponent<SpriteRenderer>().flipX = false;
+			transform.position = Vector3.MoveTowards(transform.position, pointWalkRight.transform.position, enemySpeed * Time.deltaTime);
+		}
+
+		if(isWalkingToTheLeft){
+			gameObject.GetComponent<SpriteRenderer>().flipX = true;
+			transform.position = Vector3.MoveTowards(transform.position, pointWalkLeft.transform.position, enemySpeed * Time.deltaTime);
+		}
+		
+	}
+
+
+
+
+	void OnDrawGizmos() {
+
+		Gizmos.color = Color.blue;
+
+		Gizmos.DrawWireSphere(pointWalkRight.position, WalkingRadius);
+		Gizmos.DrawWireSphere(pointWalkLeft.position, WalkingRadius);
+		Gizmos.DrawWireSphere(pointAttack.position, WalkingRadius);
+		
+	}
+}
---
+++ Damage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour {
+
+	private Transform playerTransf;
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		playerTransf = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "Player" && other.isTrigger != true && playerTransf.transform.position.x > transform.position.x){
+			other.GetComponent<Hero>().Damage(1);  // Acessa funçoes de outra classe. "Classe Hero"
+			other.GetComponent<Hero>().KnockbackRight();
+		}
+	}
+}
