3851aa5452831a21ae1e0491fbbfecdc61d00a64
unknown <thiagoex377@gmail.com>
2020-03-02 10:19:56 -0300

    V1

---
+++ Assets/C Scripts/Damage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour {
+	private Transform playerTransf;
+	void Start () {
+		playerTransf = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "Player" && other.isTrigger != true && playerTransf.transform.position.x > transform.position.x){
+			other.GetComponent<Player>().Damage(1);  
+			other.GetComponent<Player>().KnockbackRight();
+		}
+		else if(other.tag == "Player" && other.isTrigger != true && playerTransf.transform.position.x < transform.position.x){
+			other.GetComponent<Player>().Damage(1); 
+			other.GetComponent<Player>().KnockbackLeft();
+		}
+	}
+}
---
+++ Assets/C Scripts/Player.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	private Transform tr;
+	private Animator an;
+
+	public bool isWalking;
+	public bool facingRight;
+
+	private float axis;
+	public float vel;
+
+	public float jumpForce;
+	public Transform GroundCheck;
+	public bool onTheGround;
+	public float onTheGroundRadius;
+
+	public LayerMask solid;
+
+	public int curHealth;
+	public int maxHealth;
+
+	public bool death;
+
+	public float knockback;
+	public float knockbackCount;
+	public float knockbackLength;
+	public bool knockbackConfirm;
+
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D>();
+		tr = GetComponent<Transform>();
+		an = GetComponent<Animator>();
+		facingRight = true;
+
+		curHealth = maxHealth;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		onTheGround = Physics2D.OverlapCircle(GroundCheck.position, onTheGroundRadius, solid);
+
+		axis = Input.GetAxisRaw("Horizontal");
+		isWalking = Mathf.Abs(axis) > 0f;
+
+		if(axis > 0f && !facingRight){
+			Flip();
+		} else if( axis < 0f && facingRight){
+			Flip();
+		}
+
+		Animations();
+
+		if(Input.GetButtonDown("Jump") && onTheGround){
+			rb.AddForce(tr.up * jumpForce);
+		}
+
+		if(curHealth > maxHealth){
+			curHealth = maxHealth;
+		}
+		if(curHealth <= 0){
+			death = true;
+		}
+
+		if(knockbackConfirm){
+			knockbackCount -= Time.deltaTime;
+		}
+		if(knockbackCount <= 0){
+			knockbackConfirm = false;
+		}
+		
+	}
+
+	void FixedUpdate () {
+		if(!death && !knockbackConfirm){
+		if(isWalking && facingRight){
+			rb.velocity = new Vector2(vel, rb.velocity.y);
+		} else if(isWalking && !facingRight){
+				rb.velocity = new Vector2(-vel, rb.velocity.y);
+		}
+		}
+	}
+
+	void Flip(){
+		facingRight = !facingRight;
+		tr.localScale = new Vector2(-tr.localScale.x, tr.localScale.y);
+	}
+
+	void Animations(){
+		an.SetBool("Walking", isWalking);
+		an.SetFloat("VerticalVel", rb.velocity.y);
+		an.SetBool("Death", death);
+	}
+
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(GroundCheck.position, onTheGroundRadius);
+	}
+
+	public void Damage(int dmg){
+		curHealth -= dmg;
+	}
+
+	public void KnockbackRight(){
+		if(death == false){
+			rb.velocity = new Vector2(knockback, knockback*0);
+			knockbackCount = knockbackLength;
+			knockbackConfirm = true;
+		}
+	}
+
+		public void KnockbackLeft(){
+			if(death == false){
+				rb.velocity = new Vector2(-knockback, knockback*0);
+				knockbackCount = knockbackLength;
+				knockbackConfirm = true;
+			}
+		}
+}
